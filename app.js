4bc53de4e29796a8ac6a560c798cede02c6f55f1
jaycee-github <jayceewakanda@gmail.com>
2023-07-26 00:03:30 +0800

    added few fixes

--- app.js
+++ app.js
@@ -42,22 +42,11 @@ app.use(cors());
 app.use(xss());
 app.use(mongoSanitize());
 
-app.use(morgan("tiny"));
+// app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.static("./public"));
 app.use(fileUpload());
-
-// routes
-app.get("/", (req, res) => {
-    res.send("E-Commerce API");
-});
-
-app.get("/api/v1", (req, res) => {
-    // console.log(req.cookies);
-    console.log(req.signedCookies);
-    res.send("E-Commerce API");
-});
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
